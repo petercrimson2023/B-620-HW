6de25bc2927c47d83ff499cb5b146aa7db41505b
petercrimson2023 <crimsonpeter317@gmail.com>
2024-04-09 22:48:46 -0400

    finishing first draft of HW4

--- test_on_target_data.R
+++ test_on_target_data.R
@@ -27,18 +27,33 @@ data_B = merged_data %>% filter(Treatment != "A") %>% mutate(Treatment = ifelse(
                                                              Pickups2= Pickups^2,
                                                              log_Tot_Scr= log(Tot.Scr.Time),
                                                              sex_Tot_Scr = sex*Tot.Scr.Time,
-                                                             age_Tot_Scr = age*Tot.Scr.Time)
+                                                             age_Tot_Scr = age*Tot.Scr.Time,
+                                                             Day_num = as.factor(Day)%>% as.numeric())
 
 
-covariates_A = c("Pickups","Age_Pickup","sex","Phase")
+covariates_A = c("sex","Phase","Age_Pickup","Pickups")
 psm_A = PSM$new(data_A,"Treatment",covariates_A,"Tot.Soc.Time")
 psm_A$psm_analysis()
 
-covariates_B = c("Pickups","sex_Tot_Scr","age_Tot_Scr","Phase")
+
+covariates_B = c("sex","age","pets","siblings")
 psm_B = PSM$new(data_B,"Treatment",covariates_B,"Tot.Soc.Time")
 psm_B$psm_analysis()
 
 
+data_3 = merged_data %>% filter(Treatment != "P") %>%
+  mutate(Treatment = ifelse(Treatment == "A",1,0),
+         Phase = ifelse(Phase == "Treatment",1,0),
+         Exp_Age = exp(0.2*age),
+         Age2 = age^2,
+         Age_Pickup = age*Pickups,
+         Pickups2 = Pickups^2,
+         log_Tot_Scr = log(Tot.Scr.Time))
+
+covariates_3 = c("Pickups","Age_Pickup","Phase")
+
+psm_3 = PSM$new(data_3,"Treatment",covariates_3,"Tot.Soc.Time")
+psm_3$psm_analysis()
 
 
 
